b3bbe9d303cb2401a57f03ed31065f62510c9fd4
Alex Garcia <alexgarciavila@gmail.com>
2022-02-23 13:17:57 +0100

    Cambio de scraping a uso de API y creación clase gremio

--- src/__main__.py
+++ src/__main__.py
@@ -4,6 +4,8 @@ from classes.usuarios import Usuario
 from classes.gremio import Gremio
 from classes.miembros import Miembros
 from classes.json import Json
+import coreapi
+from bots import telegrambot
 
 
 def print_hi(name):
@@ -19,26 +21,76 @@ if __name__ == '__main__':
     usuario = Usuario(nombre_user, allycode, telegram_user, discord_user)
 
 
-url_guild = "https://swgoh.gg/g/708/discipulos-de-casco-oscuro/"
-sopa_guild = swgohgg_guild.preparar_sopa(url_guild)
+#url_guild = "https://swgoh.gg/g/708/discipulos-de-casco-oscuro/"
+#sopa_guild = swgohgg_guild.preparar_sopa(url_guild)
 
-url_guild2 = "https://swgoh.gg/g/oAYG8MpVSfigDgyI75C-qQ/"
-sopa_guild2 = swgohgg_guild.preparar_sopa(url_guild2)
+#url_guild2 = "https://swgoh.gg/g/oAYG8MpVSfigDgyI75C-qQ/"
+#sopa_guild2 = swgohgg_guild.preparar_sopa(url_guild2)
 
-nombre_guild = swgohgg_guild.nombre_gremio(sopa_guild)
-items_guild = swgohgg_guild.scrap_items_guild(sopa_guild)
+#nombre_guild = swgohgg_guild.nombre_gremio(sopa_guild)
+#items_guild = swgohgg_guild.scrap_items_guild(sopa_guild)
 # miembros = swgohgg_guild.scrap_miembros_guild(sopa_guild)
 # print(miembros)
-guild = Gremio(items_guild)
+#guild = Gremio(items_guild)
 
 
-nombre_guild2 = swgohgg_guild.nombre_gremio(sopa_guild2)
-items_guild2 = swgohgg_guild.scrap_items_guild(sopa_guild2)
-guild2 = Gremio(items_guild2)
+#nombre_guild2 = swgohgg_guild.nombre_gremio(sopa_guild2)
+#items_guild2 = swgohgg_guild.scrap_items_guild(sopa_guild2)
+#guild2 = Gremio(items_guild2)
 
-guild.listar_miembro(sopa_guild)
+#guild.listar_miembro(sopa_guild)
 
-swgohgg_guild.comparar_gremio(guild, guild2, nombre_guild, nombre_guild2)
+#swgohgg_guild.comparar_gremio(guild, guild2, nombre_guild, nombre_guild2)
 
-# crear_json = Json()
-# miembros_json = crear_json.crear_json(miembros)
+
+# Initialize a client & load the schema document
+client = coreapi.Client()
+schema = client.get("https://swgoh.gg/api/guild-profile/0JadhFYYRxa42RChGLFxvA/")
+datos = schema["data"]
+miembros = datos["members"]
+
+"""
+print(datos.keys())
+-------------------
+odict_keys(['guild_id', 'name', 'external_message', 'banner_color_id',
+'banner_logo_id', 'enrollment_status', 'galactic_power', 'guild_type',
+'level_requirement', 'member_count', 'members', 'avg_galactic_power',
+'avg_arena_rank', 'avg_fleet_arena_rank', 'avg_skill_rating', 'last_sync'])
+"""
+
+
+"""
+print(miembros[1].keys())
+-------------------------
+odict_keys(['galactic_power', 'guild_join_time', 'lifetime_season_score',
+'member_level', 'ally_code', 'player_level', 'player_name', 'league_id',
+'league_name', 'league_frame_image', 'portrait_image', 'title', 'squad_power'])
+
+"""
+"""
+Ejemplo
+print(schema["data"]["members"][1]["player_name"])
+"""
+"""for m in miembros:
+    print(m["player_name"])
+"""
+
+id_gremio = "0JadhFYYRxa42RChGLFxvA"
+cascos = Gremio(datos)
+print(cascos.id)
+print(cascos.nombre)
+print(cascos.texto)
+print(cascos.banner_color_id)
+print(cascos.banner_logo_id)
+print(cascos.enrollment_status)
+print(cascos.galactic_power)
+print(cascos.avg_galactic_power)
+print(cascos.guild_type)
+print(cascos.level_requirement)
+print(cascos.member_count)
+print(cascos.avg_arena_rank)
+print(cascos.avg_fleet_arena_rank)
+print(cascos.avg_skill_rating)
+print(cascos.last_sync)
+
+#telegrambot.main()
--- src/classes/gremio.py
+++ src/classes/gremio.py
@@ -1,22 +1,103 @@
-﻿from scrapers import swgohgg_guild
+﻿from unicodedata import name
+from scrapers import swgohgg_guild
 from classes.miembros import Miembros
+import coreapi
 
 
 class Gremio:
-    def __init__(self, items):
-        self.gp = items["Galactic Power"]
-        self.avggp = items["Avg Galactic Power"]
-        self.rey = items["Rey"]
-        self.slkr = items["SLKR"]
-        self.jml = items["JML"]
-        self.see = items["SEE"]
-        self.jmk = items["JMK"]
-        self.lv = items["LV"]
-        self.chargp = items["Character GP"]
-        self.shipgp = items["Ship GP"]
-        self.avgsr = items["Avg Skill Rating"]
-        self.avgarenarank = items["Avg Current Arena Rank"]
-        self.avgshiparenarank = items["Avg Current Fleet Arena Rank"]
+    def __init__(self, datos_guild):
+        self.id = self.set_id(datos_guild)
+        self.nombre = self.set_nombre(datos_guild)
+        self.texto = self.set_texto_guild(datos_guild)
+        self.banner_color_id = self.set_banner_color_id(datos_guild)
+        self.banner_logo_id = self.set_banner_logo_id(datos_guild)
+        self.enrollment_status = self.set_enrollment_status(datos_guild)
+        self.galactic_power = self.set_galactic_power(datos_guild)
+        self.avg_galactic_power = self.set_avg_galactic_power(datos_guild)
+        self.guild_type = self.set_guild_type(datos_guild)
+        self.level_requirement = self.set_level_requirement(datos_guild)
+        self.member_count = self.set_member_count(datos_guild)
+        self.avg_arena_rank = self.set_avg_arena_rank(datos_guild)
+        self.avg_fleet_arena_rank = self.set_avg_fleet_arena_rank(datos_guild)
+        self.avg_skill_rating = self.set_avg_skill_rating(datos_guild)
+        self.last_sync = self.set_last_sync(datos_guild)
+
+
+    def set_id(self, datos_guild):
+        id = datos_guild["guild_id"]
+
+        return id
+
+    def set_nombre(self, datos_guild):
+        nombre = datos_guild["name"]
+
+        return nombre
+
+    def set_texto_guild(self, datos_guild):
+        texto = datos_guild["external_message"]
+
+        return texto
+
+    def set_banner_color_id(self, datos_guild):
+        banner_color_id = datos_guild["banner_color_id"]
+
+        return banner_color_id
+
+    def set_banner_logo_id(self, datos_guild):
+        banner_logo_id = datos_guild["banner_logo_id"]
+
+        return banner_logo_id
+    
+    def set_enrollment_status(self, datos_guild):
+        enrollment_status = datos_guild["enrollment_status"]
+
+        return enrollment_status
+
+    def set_galactic_power(self, datos_guild):
+        galactic_power = datos_guild["galactic_power"]
+
+        return galactic_power
+
+    def set_avg_galactic_power(self, datos_guild):
+        avg_galactic_power = datos_guild["avg_galactic_power"]
+
+        return avg_galactic_power
+
+    def set_guild_type(self, datos_guild):
+        guild_type = datos_guild["guild_type"]
+
+        return guild_type
+
+    def set_level_requirement(self, datos_guild):
+        level_requirement = datos_guild["level_requirement"]
+
+        return level_requirement
+
+    def set_member_count(self, datos_guild):
+        member_count = datos_guild["member_count"]
+
+        return member_count
+
+    def set_avg_arena_rank(self, datos_guild):
+        avg_arena_rank = datos_guild["avg_arena_rank"]
+
+        return avg_arena_rank
+
+    def set_avg_fleet_arena_rank(self, datos_guild):
+        avg_fleet_arena_rank = datos_guild["avg_fleet_arena_rank"]
+
+        return avg_fleet_arena_rank
+
+    def set_avg_skill_rating(self, datos_guild):
+        avg_skill_rating = datos_guild["avg_skill_rating"]
+
+        return avg_skill_rating
+
+    def set_last_sync(self, datos_guild):
+        last_sync = datos_guild["last_sync"]
+
+        return last_sync
+
 
     def listar_miembro(self, sopa_guild):
         """
